728e21badf6c6d7868cb11dd283229991e8e69d5
sherckdj <sherckdj@tiffin.edu>
2025-02-05 08:56:20 -0500

    Create quiz.tsx

---
+++ src/pages/quiz.tsx
@@ -0,0 +1,9 @@
+import Quiz from "@/components/Quiz";
+
+export default function QuizPage() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <Quiz />
+    </div>
+  );
+}
